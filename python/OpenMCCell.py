Fixes to allow rotation matrices correct in OpenMC

python/OpenMCCell.py
@@ -1,8 +1,29 @@
 #/usr/env/python3
 
+import math
+import numpy as np
+
 from CellCard import CellCard
 import xml.etree.ElementTree as ET
 
+def angle_from_rotmatrix(matrix):
+
+    matrix = [float(i) for i in matrix]
+
+    # todo these -1s are guesses
+    theta_r = -1*math.asin(matrix[6])
+    theta = -1*np.rad2deg(theta_r)
+
+    # these -1s are guesses
+    sin_phi = matrix[7]/np.cos(theta_r)
+    phi = -1*np.rad2deg(math.asin(sin_phi))
+
+    # this one makes clite work properly
+    sin_psi = matrix[3] / np.cos(theta_r)
+    psi = -1*np.rad2deg(math.asin(sin_psi))
+
+    return phi,theta,psi
+                     
 # turn the generic operation type into a mcnp relevant text string
 def openmc_op_from_generic(Operation):
     # if we are not of type operator - we are string do nowt
@@ -40,9 +61,14 @@ def get_openmc_cell_info(cell):
 
     if cell.cell_universe_rotation != 0:
         rotation = ""
-        rotation += str(cell.cell_universe_rotation[0]) + " "
-        rotation += str(cell.cell_universe_rotation[4]) + " "
-        rotation += str(cell.cell_universe_rotation[8])
+
+        [phi,theta,psi] = angle_from_rotmatrix(cell.cell_universe_rotation)
+
+        print (cell.cell_id, phi, theta, psi)
+        
+        rotation += str(phi) + " "
+        rotation += str(theta) + " "
+        rotation += str(psi)
     else:
         rotation = "0 0 0"
 