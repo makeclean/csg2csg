02c0ec5a30a0edaa9b028362f6505b762a698d39
shimwell <mail@jshimwell.com>
2019-07-16 12:14:06 +0100

    changed nuc lib id to 31c instead of nothing

--- python/test/UnitTestMCNPMaterial.py
+++ python/test/UnitTestMCNPMaterial.py
@@ -11,7 +11,7 @@ class TestMCNPMaterial(unittest.TestCase):
 
     def test_mcnp_material(self):
         string ="29063 6.917000e-01 \n" + \
-                "29065 3.083000e-01 \n"
+                "29065.31c 3.083000e-01 \n"
         number = 1
         name = "M1"
         matcard = MCNPMaterialCard(number,string)
@@ -30,12 +30,12 @@ class TestMCNPMaterial(unittest.TestCase):
         self.assertEqual(list(matcard.composition_dictionary.values())[1],3.083000e-01)
         
         self.assertEqual(len(matcard.xsid_dictionary),2)
-        
+
         self.assertEqual(list(matcard.xsid_dictionary.keys())[0],'29063')
         self.assertEqual(list(matcard.xsid_dictionary.keys())[1],'29065')
 
         self.assertEqual(list(matcard.xsid_dictionary.values())[0],'')
-        self.assertEqual(list(matcard.xsid_dictionary.values())[1],'')
+        self.assertEqual(list(matcard.xsid_dictionary.values())[1],'31c')
 
 if __name__ == '__main__':
     unittest.main()
