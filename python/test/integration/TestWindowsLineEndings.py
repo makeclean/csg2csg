412e33527fb790199a7e53a7f807755e07d26087
Steve Lilley <py1sl@users.noreply.github.com>
2019-08-01 11:22:13 +0100

    updated for new arg parse variables

--- python/test/integration/TestWindowsLineEndings.py
+++ python/test/integration/TestWindowsLineEndings.py
@@ -5,10 +5,10 @@ import subprocess
 
 class TestWindowsLineEndings(unittest.TestCase):   
     def test_windows_line_endings(self):
-        return_code = subprocess.call("python3 ../../csg2csg.py -f files/spheres.i -o all", shell=True)  
+        return_code = subprocess.call("python3 ../../csg2csg.py -i files/spheres.i -o all", shell=True)  
         self.assertEqual(return_code,0)
         return_code = subprocess.call("sed -e 's/$/\r/' files/spheres.i > spheres_win.i                # UNIX to DOS  (adding CRs)", shell=True)
-        return_code = subprocess.call("python3 ../../csg2csg.py -f spheres_win.i -o all", shell=True)  
+        return_code = subprocess.call("python3 ../../csg2csg.py -i spheres_win.i -o all", shell=True)  
         self.assertEqual(return_code,0)
 
 if __name__ == '__main__':
