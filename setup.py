06e022ddf4a4e2313525235a954cf978d5d29797
shimwell <mail@jshimwell.com>
2019-08-02 18:56:51 +0100

    changed author name

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="csg2csg",
-    version="0.0.26",
-    author="makeclean",
+    version="0.0.27",
+    author="andrew davis",
     author_email="andrew.davis@ukaea.uk",
     description="Convert CSG geometry into different formats",
     long_description=long_description,
