9d148657aa1cf2fb60448c066ceb0ce3558eddf4
Andrew Davis <andrew.davis@ukaea.uk>
2021-12-02 19:36:18 +0000

    Add roundtrip test for an MCNP file

--- csg2csg/test/integration/TestWindowsLineEndings.py
+++ csg2csg/test/integration/TestWindowsLineEndings.py
@@ -12,5 +12,14 @@ class TestWindowsLineEndings(unittest.TestCase):
         return_code = subprocess.call("csg2csg -i spheres_win.i -f mcnp -o all", shell=True)
         self.assertEqual(return_code,0)
 
+    # round trip the MCNP file produced from the line above - it should be
+    # identical
+    def test_round_trip(self):
+        return_code = subprocess.call("csg2csg -i mcnp/file.mcnp -f mcnp -o mcnp", shell=True)
+        self.assertEqual(return_code,0)
+
+        return_code = subprocess.call("diff mcnp/file.mcnp mcnp/mcnp/file.mcnp", shell=True)
+        self.assertEqual(return_code,0)
+        
 if __name__ == '__main__':
     unittest.main()
